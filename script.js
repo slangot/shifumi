a77557e2e2bca821eff2d43443ca6bb8b1cd5dd7
Shawn <shawn17@live.fr>
2021-06-10 23:16:44 +0200

    Modification de l'index en enlevant le bloc adversaire de choix, création des animations des mains dans le modal, création du fichier script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+/****** Choice Container */
+const choiceElement = document.querySelectorAll('.choice-element');
+
+/****** Fight Container */
+const fightContainer =  document.querySelector('.fight-container');
+let userChoiceFight = document.querySelector('.user-choice-fight');
+let opponentChoiceFight = document.querySelector('.opponent-choice-fight');
+
+choiceElement.addEventListener('click', (event) => {
+    console.log("ok");
+});
